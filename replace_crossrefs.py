dc813c8c8a25ad4e2e4c75c1c184f2d2d7f3c03a
William F. Broderick <billbrod@gmail.com>
2026-04-15 14:39:43 -0400

    update header

--- replace_crossrefs.py
+++ replace_crossrefs.py
@@ -25,7 +25,7 @@ look weird.
 
 :::"""
 
-nb_txt = re.sub(r":::{admonition} Download.*?:::", warning, nb_txt, flags=re.DOTALL)
+nb_txt = re.sub(r":::{admonition} Run this notebook yourself.*?:::", warning, nb_txt, flags=re.DOTALL)
 
 for sphinx_domain, tilde, match in re.findall(r"({.*?})`(~?)([a-z-A-Z_\.]+?)`", nb_txt):
     if tilde:
